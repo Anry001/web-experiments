12a8763b8821b35d3e94e258cb1a1b4237485040
anry <anryclass@gmail.com>
2023-04-08 02:58:27 +0300

    feat(app): implemented routes in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import Box from '@mui/material/Box';
 import { Stack, Button } from '@mui/material';
 import ShitDataGrid from './components/ShitDataGrid';
+import AppRoutes from './AppRoutes';
 
 const App = () => {
-  return (
-  );
+  return <AppRoutes />;
 };
 
 export default App;
