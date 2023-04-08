c105f8c11f9d7e76cfb5d5ba7d6210bc4a2614e0
anry <anryclass@gmail.com>
2023-04-08 03:04:23 +0300

    build: fixed file locations in folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
-import Box from '@mui/material/Box';
-import { Stack, Button } from '@mui/material';
-import ShitDataGrid from './components/ShitDataGrid';
-import Routes from './pages/app-routes/Routes';
+import Routes from './app-routes/Routes';
 
 const App = () => {
   return <Routes />;
