f4a69e221ad0096d5d9f36a72b1fc0ac06e269cc
anry <anryclass@gmail.com>
2023-04-08 17:36:59 +0300

    feat(home): added buttons to link to about page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,35 @@
-import { Stack } from '@mui/material';
+import { Stack, Button } from '@mui/material';
 import ShitDataGrid from '@components/ShitDataGrid';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+  const HELLO_WORLD_BUTTON_TEXT = 'Click to go to hello world';
+
   return (
-    <Stack
-      direction="row"
-      justifyContent="space-evenly"
-      alignItems="center"
-      spacing={2}
-    >
-      <ShitDataGrid />
-      <ShitDataGrid />
-      <ShitDataGrid />
+    <Stack>
+      <Stack
+        sx={{ background: 'yellow' }}
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+        spacing={2}
+      >
+        <ShitDataGrid />
+        <ShitDataGrid />
+        <ShitDataGrid />
+      </Stack>
+      <Stack
+        sx={{ mt: '100px' }}
+        direction="row"
+        justifyContent="space-evenly"
+        alignItems="center"
+        spacing={10}
+      >
+        <Link to="about" style={{ textDecoration: 'none' }}>
+          <Button variant="contained">{HELLO_WORLD_BUTTON_TEXT}</Button>
+        </Link>
+        <Button variant="contained">Click me</Button>
+      </Stack>
     </Stack>
   );
 };
