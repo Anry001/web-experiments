7cd3297d0c564fee969f9055489d671c60f6ee74
anry <anryclass@gmail.com>
2023-04-08 18:32:47 +0300

    feat(contact): added contact page

--- src/app-routes/Routes.tsx
+++ src/app-routes/Routes.tsx
@@ -1,10 +1,12 @@
-import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from '@pages/Home';
 import About from '@/pages/About';
+import Contact from '@/pages/Contact';
 
 const routes = createBrowserRouter([
   { path: '/', element: <Home /> },
   { path: '/about', element: <About /> },
+  { path: '/contact', element: <Contact /> },
 ]);
 
 const Routes = () => {
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,32 @@
+import { Button, Stack, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const BACK_TO_HOME_BUTTON_TEXT = 'Home page';
+
+const Contact = () => {
+  return (
+    <Stack
+      sx={{ height: '60vh', width: '100vw' }}
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Typography variant="h2" gutterBottom>
+        contact info below:
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        email: someemail@gmail.con
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        phone: 123-456-7890
+      </Typography>
+      <Typography variant="h5" gutterBottom>
+        name: john doe
+      </Typography>
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <Button variant="contained">{BACK_TO_HOME_BUTTON_TEXT}</Button>
+      </Link>
+    </Stack>
+  );
+};
+export default Contact;
