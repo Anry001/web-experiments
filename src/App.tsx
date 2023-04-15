1bc09344f237a4e80bd2dcc60ae0f9c04a9d2218
anry <anryclass@gmail.com>
2023-04-16 00:45:06 +0300

    build(app): added query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import Routes from './app-routes/Routes';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
-  return <Routes />;
+  <QueryClientProvider client={queryClient}>
+    return <Routes />;
+  </QueryClientProvider>;
 };
 
 export default App;
