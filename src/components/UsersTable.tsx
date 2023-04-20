0f5c2a364d665fe8b29035f48a4d3264e8f14fdb
anry <anryclass@gmail.com>
2023-04-20 20:15:23 +0300

    refactor

--- src/components/UsersTable.tsx
+++ src/components/UsersTable.tsx
@@ -18,7 +18,7 @@ const ShitDataGrid = () => {
       direction="row"
       justifyContent="center"
       alignItems="center"
-      spacing={4}
+      spacing={2}
     >
       <DataGrid
         rows={data ?? []}
@@ -29,9 +29,7 @@ const ShitDataGrid = () => {
         experimentalFeatures={{ newEditingApi: true }}
         loading={isLoading}
       />
-      <Box sx={{ pb: '200px' }}>
-        <AddUser />
-      </Box>
+      <AddUser />
     </Stack>
   );
 };
