7ad428c7b5a79bd9bec1e6556114d0df4ea7192a
anry <anryclass@gmail.com>
2023-04-20 20:15:05 +0300

    feat(add-user): added text fields

--- src/components/AddUser.tsx
+++ src/components/AddUser.tsx
@@ -1,11 +1,19 @@
-import { Stack, Typography } from '@mui/material';
+import { Stack, TextField, Typography } from '@mui/material';
 
 const AddUser = () => {
   return (
-    <Stack>
+    <Stack
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      spacing={1}
+    >
       <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
         Add new user:
       </Typography>
+      <TextField id="outlined-basic" label="User Name" variant="outlined" />
+      <TextField id="outlined-basic" label="User Name" variant="outlined" />
+      <TextField id="outlined-basic" label="User Name" variant="outlined" />
     </Stack>
   );
 };
