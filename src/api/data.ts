eb83b552f565a6594beb0c7cc927791e38687dc9
anry <anryclass@gmail.com>
2023-04-22 00:29:46 +0300

    feat(data): added users data

---
+++ src/api/data.ts
@@ -0,0 +1,5 @@
+export const users = [
+  { id: 2, name: 'kaka', age: 23 },
+  { id: 3, name: 'lk', age: 50 },
+  { id: 4, name: 'kak', age: 2 },
+];
--- src/api/getUsers.ts
+++ src/api/getUsers.ts
@@ -1,16 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
+import { users } from './data';
 
 interface User {
   id: number;
   name: string;
   age: number;
 }
-
-const users = [
-  { id: 2, name: 'kaka', age: 23 },
-  { id: 3, name: 'lk', age: 50 },
-  { id: 4, name: 'kak', age: 2 },
-];
 
 const getUsers = (): Promise<User[]> => {
   return new Promise<User[]>((resolve, reject) => {
