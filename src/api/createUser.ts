e59e730fe5f83773babebf3a0ad381dc4e7b1a2c
anry <anryclass@gmail.com>
2023-04-22 02:27:03 +0300

    build(create-user): updated import url

--- src/api/createUser.ts
+++ src/api/createUser.ts
@@ -1,5 +1,5 @@
 import { useMutation } from '@tanstack/react-query';
-import { User, users } from './data';
+import { User, users } from '@data/data';
 
 const createUser = async (user: User): Promise<User> => {
   const newUser: User = { ...user };
