27ba5cb35bfdf1f0d6688e0c79542ff81f0dfd16
anry <anryclass@gmail.com>
2023-04-23 21:37:24 +0300

    refactor(get-users): refactored code

--- src/api/getUsers.ts
+++ src/api/getUsers.ts
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import { User, users } from '@data/data';
 
-const getUsers = async (): Promise<User[]> => {
+export const getUsers = async (): Promise<User[]> => {
   return users;
 };
 
-export const useGetUsersQuery = () => {
-  return useQuery(['users'], getUsers);
-};
+// export const useGetUsersQuery = () => {
+//   return useQuery({ queryKey: ['users'], queryFn: getUsers });
+// };
