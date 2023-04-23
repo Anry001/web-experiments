4b0dda3d6106701c4c336d177b40684b8ec311ec
anry <anryclass@gmail.com>
2023-04-23 21:37:00 +0300

    feat(create-user): configured create user method

--- src/api/createUser.ts
+++ src/api/createUser.ts
@@ -1,12 +1,32 @@
-import { useMutation } from '@tanstack/react-query';
+// import { useMutation } from '@tanstack/react-query';
 import { User, users } from '@data/data';
 
-const createUser = async (user: User): Promise<User> => {
-  const newUser: User = { ...user };
+// export interface User {
+//   id: string;
+//   name: string;
+//   age: string;
+// }
+
+// export let users: User[] = [
+//   { id: '2', name: 'faf', age: '23' },
+//   { id: '3', name: 'lk', age: '50' },
+//   { id: '4', name: 'kak', age: '2' },
+// ];
+
+export const createUser = async (user: User): Promise<User> => {
+  let newUser: User = user;
+  console.log(newUser);
   users.push(newUser);
+  console.log(users);
   return newUser;
 };
 
-export const useCreateUserMutation = () => {
-  return useMutation(['users'], createUser);
-};
+// export const useCreateUserMutation = () => {
+//   return useMutation({
+//     mutationKey: ['users'],
+//     mutationFn: createUser,
+//     onSuccess: (data) => {
+//       console.log(data);
+//     },
+//   });
+// };
